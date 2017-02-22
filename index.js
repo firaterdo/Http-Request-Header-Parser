db4dd51bfe832f7906958873c2ecd6c2f3e7941a
Firat Erdogmus <firaterdogmus@gmail.com>
2017-02-22 12:27:47 +0000

    bu da bu sekil

--- index.js
+++ index.js
@@ -3,19 +3,11 @@ var app = express()
 
 app.set('port', (process.env.PORT || 5000));
 
-
-
-
 app.get('/', function (req, res) {
-  
 
-  res.send('Welcome larca <br>')
+res.send('Welcome larca <br>' + req.ip)
 
-  
 })
-
-
-
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
